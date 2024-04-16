b977cd6fae8f02c7aa6a76a8bb78d034bfc8c851
ks4wI <148214067+ks4wI@users.noreply.github.com>
2024-04-16 15:17:46 +0200

    Add files via upload

---
+++ wyszbinarne/wyszbinarne/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class BinarySearch
+{
+    static void Main()
+    {
+        Console.WriteLine("Wprowadź posortowaną tablicę liczb całkowitych, oddzielając je przecinkami:");
+        string[] input = Console.ReadLine().Split(',');
+        int[] array = Array.ConvertAll(input, int.Parse);
+
+        Console.WriteLine("Wprowadź liczbę, którą chcesz znaleźć:");
+        int target = int.Parse(Console.ReadLine());
+
+        int result = BinarySearchAlgorithm(array, target);
+
+        if (result != -1)
+        {
+            Console.WriteLine($"Liczba {target} została znaleziona na pozycji {result + 1} w tablicy.");
+        }
+        else
+        {
+            Console.WriteLine($"Liczba {target} nie została znaleziona w tablicy.");
+        }
+    }
+
+    static int BinarySearchAlgorithm(int[] array, int target)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target)
+            {
+                return mid;
+            }
+            else if (array[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
